b7f1862f1e319e0a9c288cfb0927d575f5fecc89
Tomasz Woźniak <tomasz.wozn@gmail.com>
2015-11-03 23:54:21 +0100

    initial commit

---
+++ src/com/eartrainer/activity/MainScreen.java
@@ -0,0 +1,14 @@
+package com.eartrainer.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.eartrainer.R;
+
+public class MainScreen extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
