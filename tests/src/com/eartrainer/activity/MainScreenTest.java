a476038149459068383d202acbea56d4f363990d
Tomasz Woźniak <tomasz.wozn@gmail.com>
2015-11-15 22:35:23 +0100

    main screen unit test

--- tests/src/com/eartrainer/activity/MainScreenTest.java
+++ tests/src/com/eartrainer/activity/MainScreenTest.java
@@ -1,6 +1,10 @@
 package com.eartrainer.activity;
 
+import android.content.Intent;
 import android.test.ActivityUnitTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.widget.Button;
+import com.eartrainer.R;
 
 /**
  * This is a simple framework for a test of an Application.  See
@@ -14,8 +18,43 @@ import android.test.ActivityUnitTestCase;
  */
 public class MainScreenTest extends ActivityUnitTestCase<MainScreen> {
 
+    private Intent launchMainIntent;
+
     public MainScreenTest() {
         super(MainScreen.class);
     }
 
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        launchMainIntent = new Intent(getInstrumentation().getTargetContext(), MainScreen.class);
+        startActivity(launchMainIntent, null, null);
+    }
+
+    @MediumTest
+    public void testTonesRecognitionActivityWasLaunchedWithIntent() {
+        final Button buttonLaunchTonesRecognition = (Button) getActivity().findViewById(R.id.btn_tones);
+        buttonLaunchTonesRecognition.performClick();
+
+        final Intent launchIntent = getStartedActivityIntent();
+        assertNotNull("Intent was null", launchIntent);
+    }
+
+    @MediumTest
+    public void testEqualizationRecognitionActivityWasLaunchedWithIntent() {
+        final Button buttonLaunchEqRecognition = (Button) getActivity().findViewById(R.id.btn_eq);
+        buttonLaunchEqRecognition.performClick();
+
+        final Intent launchIntent = getStartedActivityIntent();
+        assertNotNull("Intent was null", launchIntent);
+    }
+
+    @MediumTest
+    public void testViewResultsActivityWasLaunchedWithIntent() {
+        final Button buttonLaunchViewResultsActivity = (Button) getActivity().findViewById(R.id.btn_results);
+        buttonLaunchViewResultsActivity.performClick();
+
+        final Intent launchIntent = getStartedActivityIntent();
+        assertNotNull("Intent was null", launchIntent);
+    }
 }
